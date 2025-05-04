26ee2e08e7b807d8eb6af6c4e9f7a474e060e51e
Darina Stoyanova-Mateva <darina.stoyanovamateva@gmail.com>
2025-05-04 10:36:25 +0000

    append three lines

---
+++ twelveappendfile.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "My favorite movie is \"Friends\"" >./filecreatedwithscript.txt
+
+echo "My favorite book is think and grow rich" >>./filecreatedwithscript.txt
+
+echo "My favorite suv is range rover" >> ./filecreatedwithscript.txt
