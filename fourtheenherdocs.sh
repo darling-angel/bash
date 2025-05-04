f40dfb768639f62fe6ed153f7e0545ca1f809482
Darina Stoyanova-Mateva <darina.stoyanovamateva@gmail.com>
2025-05-04 20:23:18 +0000

    long comment and first variable

---
+++ fourtheenherdocs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+<<LINUXHINT_MESSAGE
+echo "My favorite movie is \"Friends\"" > ./filecreatedwithscript.txt
+
+this is a comment
+echo "My favorite book is think and grow rich" # >> ./filecreatedwithscript.txt
+LINUXHINT_MESSAGE
+
+echo "My favorite suv is range rover" > ./filecreatedwithscript.txt
+
