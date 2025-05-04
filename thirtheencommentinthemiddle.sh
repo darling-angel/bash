cb3037783533d64da3b62921b426e9e7ae591d88
Darina Stoyanova-Mateva <darina.stoyanovamateva@gmail.com>
2025-05-04 15:45:24 +0000

    comment in the middle of the line

---
+++ thirtheencommentinthemiddle.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "My favorite movie is \"Friends\"" > ./filecreatedwithscript.txt
+echo "My favorite book is think and grow rich" # >> ./filecreatedwithscript.txt
+echo "My favorite suv is range rover" >> ./filecreatedwithscript.txt
+
